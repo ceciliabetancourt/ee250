fa948179ad2c1243870dccfe2ccfd1c0f5d1daab
leire <lizarral@usc.edu>
2025-10-12 15:44:03 -0700

    Update and rename main.py to lab5/main.py

--- lab5/main.py
+++ lab5/main.py
@@ -12,18 +12,6 @@ def get_wifi_signal_strength() -> int:
     Returns:
         The signal strength in dBm.
     """
-    # Question 1: What is dBm? What values are considered good and bad for WiFi signal strength?
-
-    # Question 2: Why do we need to check the OS? What is the difference between the commands for each OS?
-
-    # Question 3: In your own words, what is subprocess.check_output doing? What does it return?
-    # HINT: https://docs.python.org/3/library/subprocess.html#subprocess.check_output
-
-    # Question 4: In your own words, what is re.search doing? What does it return?
-    # HINT: https://docs.python.org/3/library/re.html#re.search
-
-    # Question 5: In the Windows case, why do we need to convert the signal quality to dBm?
-    # HINT: https://learn.microsoft.com/en-us/windows/win32/api/wlanapi/ns-wlanapi-wlan_association_attributes?redirectedfrom=MSDN
     if platform.system() == 'Linux': # Linux
         output = subprocess.check_output("iwconfig wlan0", shell=True)
         match = re.search(r"Signal level=(-?\d+) dBm", output.decode('utf-8'))
@@ -34,8 +22,8 @@ def get_wifi_signal_strength() -> int:
         signal_quality = int(match.group(1))
         signal_strength = -100 + signal_quality / 2
     elif platform.system() == 'Darwin': # Mac
-        output = subprocess.check_output("/System/Library/PrivateFrameworks/Apple80211.framework/Versions/Current/Resources/airport -I", shell=True)
-        match = re.search(r"agrCtlRSSI:\s*(-?\d+)", output.decode('utf-8'))
+        output = subprocess.check_output("wdutil info", shell=True)
+        match = re.search(r"RSSI\s+:\s*(-?\d+)", output.decode('utf-8'))
         signal_strength = int(match.group(1))
     else:
         raise Exception("Unknown OS")
@@ -45,7 +33,7 @@ def get_wifi_signal_strength() -> int:
 def main():
     # Choose at least 5 locations to sample the signal strength at
     # These can be rooms in your house, hallways, different floors, outside, etc. (as long as you can get a WiFi signal)
-    locations = ['bedroom', 'living room', 'kitchen', 'bathroom', 'garage']
+    locations = ['Bedroom', 'Bathroom', 'Kitchen', 'Front Door', 'Living Room'] # doing diff locations in my room
     samples_per_location = 10 # number of samples to take per location
     time_between_samples = 1 # time between samples (in seconds)
 
@@ -57,12 +45,16 @@ def main():
 
         # TODO: collect 10 samples of the signal strength at this location, waiting 1 second between each sample
         # HINT: use the get_wifi_signal_strength function
+        for i in range(samples_per_location):
+            strength = get_wifi_signal_strength()
+            signal_strengths.append(strength)
+            time.sleep(time_between_samples)
+            
         
         # TODO: calculate the mean and standard deviation of the signal strengths you collected at this location
-        signal_strength_mean = None
-        signal_strength_std = None
+        signal_strength_mean = sum(signal_strengths)/len(signal_strengths)
+        signal_strength_std = np.std(signal_strengths)
 
-        # Question 6: What is the standard deviation? Why is it useful to calculate it?
         data.append((location, signal_strength_mean, signal_strength_std))
 
     # create a dataframe from the data
@@ -78,15 +70,15 @@ def main():
     # NOTE: use the error_y parameter of px.bar to plot the error bars (1 standard deviation)
     #   documentation: https://plotly.com/python-api-reference/generated/plotly.express.bar.html
     fig = px.bar(
-        
-    )
-    # Question 8: Why is it important to plot the error bars? What do they tell us?
+        data_frame=df,
+        x='location',
+        y='signal_strength_mean',
+        error_y='signal_strength_std',
+        title='Wi-Fi Signal Strength by Location'
+        )
 
     # write the plot to a file - make sure to commit the PNG file to your repository along with your code
-    fig.write_image("signal_strength.png")
-
-    # Question 9: What did you observe from the plot? How does the signal strength change as you move between locations?
-    #             Why do you think signal strength is weaker in certain locations?
+    fig.show()
 
 
 if __name__ == "__main__":
