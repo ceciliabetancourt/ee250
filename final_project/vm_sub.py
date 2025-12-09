c9d1d1a7a3950d833fa558386c86f790926bc138
leire <lizarral@usc.edu>
2025-12-08 16:10:52 -0800

    Update vm_sub.py

--- final_project/vm_sub.py
+++ final_project/vm_sub.py
@@ -1,35 +1,50 @@
 import paho.mqtt.client as mqtt
+import threading
+import time
 
-# function (or "callback") executed when client receives a connection acknowledgement 
-# packet response from the server
+# define global variables
+latest_city = None
+client = None
+
+# (1) mqtt broker functions
 def on_connect(client, userdata, flags, rc):
-    print("Connected to server (i.e., broker) with result code "+str(rc))
-    
-    # replace user with your USC username in all subscriptions
     client.subscribe("lizarral/cityinfo")
-    
-    # add the custom callbacks by indicating the topic and the name of the callback handle
     client.message_callback_add("lizarral/cityinfo", on_message_from_cityinfo)
-    
-def on_message(client, userdata, msg):
-    print("Default callback - topic: " + msg.topic + "   msg: " + str(msg.payload, "utf-8"))
+    print("Connected to broker with result code", rc)
 
-# custom message callback
 def on_message_from_cityinfo(client, userdata, message):
-   global latest_city
-   print("Custom callback  - City: "+message.payload.decode())
-   latest_city = message.payload.decode()
+    global latest_city
+    latest_city = message.payload.decode()
+    print("Custom callback - City:", latest_city)
 
-if __name__ == '__main__':
-    # create a client object
+# (2) defined functions used to avoid race conditions (specifically, the possibility of 
+# 'weather_api.py' grabbing 'latest_city' when it's still defined as None
+def start_mqtt(background=True): # (AIDED BY CHATGPT)
+    # starts the mqtt client
+    global client
+    if client is not None:
+        return client  # already started
     client = mqtt.Client()
-
-    # attach a default callback which we defined above for incoming mqtt messages
-    client.on_message = on_message
-
-    # attach the on_connect() callback function defined above to the mqtt client
     client.on_connect = on_connect
+    client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
 
-    # connect to broker
-    client.connect(host="broker.hivemq.com", port=1883, keepalive=60)
-    client.loop_start()
+    if background:
+        thread = threading.Thread(target=client.loop_forever, daemon=True)
+        thread.start()
+    else:
+        client.loop_forever() # blocks
+    return client
+
+def wait_for_city(timeout=None, poll_interval=0.1):
+    # ensures that the variable 'latest_city' is not grabbed until it's not 
+    # None or it times out!
+    start = time.time()
+    while latest_city is None:
+        if timeout is not None and time.time() - start > timeout:
+            return None
+        time.sleep(poll_interval)
+    return latest_city
+
+if __name__ == "__main__":
+    start_mqtt(background=False)
+    
