7e13bcc42ed97bdc78b0021facc158dcec218c32
leire <lizarral@usc.edu>
2025-12-08 16:10:17 -0800

    Update rpi_pub.py

--- final_project/rpi_pub.py
+++ final_project/rpi_pub.py
@@ -1,12 +1,7 @@
-# grovepi libraries
-import sys
-sys.path.append('~/Dexter/GrovePi/Software/Python')
-import grovepi
-# mqtt broker libraries
 import paho.mqtt.client as mqtt
 import time
-from datetime import datetime
-import socket
+import sys
+import grovepi
 
 # potentiometer connected to analog port A0 as input
 potentiometer = 0
@@ -16,48 +11,47 @@ grovepi.pinMode(potentiometer,"INPUT")
 light_sensor = 1
 grovepi.pinMode(light_sensor,"INPUT")
 
-# callback executed when client receives a connection acknowledgement 
-# packet response from the server
+# mqtt callback executed when client receives a connection acknowledgement 
 def on_connect(client, userdata, flags, rc):
     print("Connected to server (i.e., broker) with result code "+str(rc))
 
 if __name__ == '__main__':
     # (1) publish to mqtt broker
-    # get IP address
-    ip_address = 0
-    hostname = socket.gethostname()
-    ip_address = socket.gethostbyname(hostname)
-    
     # create a client object
     client = mqtt.Client()
-    
+
     # attach the on_connect() callback function defined above to the mqtt client
     client.on_connect = on_connect
-    client.connect(host="broker.hivemq.com", port=1883, keepalive=60)
-
+    client.connect(host="test.mosquitto.org", port=1883, keepalive=60)
+    
     # separate thread to handle incoming, outgoing mqtt messages 
     client.loop_start()
-    time.sleep(1)
 
     while True:
-        # publish chosen city and replace user with your USC username in all subscriptions
-        # (2) read potentiometer value
+        # publish chosen city and color of display :)
+        # (2) read light sensor value
+        light_sensor_val = grovepi.analogRead(light_sensor)
+        print('Light sensor –> ', f"{light_sensor_val}")
+        time.sleep(0.2)
+        if light_sensor_val < 260: # through experiments, we determined divisions as 0-259, 260-519, >520
+            display = 'yellow'
+        elif light_sensor_val < 520:
+            display = 'blue'
+        else: # light_sensor_val >= 520:
+            display = 'red'
+        client.publish("lizarral/displayinfo", f"{display}")
+        time.sleep(1)
+
+        # (3) read potentiometer value
         potentiometer_val = grovepi.analogRead(potentiometer)
+        print('Potentiometer –> ', f"{potentiometer_val}")
+        time.sleep(0.2)
         if potentiometer_val < 341: # since it's from 0-1023, divisions are 0-341, 342-682, 683-1023
             city = 'Madrid'
         elif potentiometer_val < 682:
             city = 'Los Angeles'
-        else: # potentiometer_val > 682:
+        else: # potentiometer_val >= 682:
             city = 'London'
         client.publish("lizarral/cityinfo", f"{city}")
-        time.sleep(4)
-        # (3) read light sensor value
-        light_sensor_val = grovepi.analogRead(light_sensor)
-        if light_sensor_val < 341: # since it's from 0-1023, divisions are 0-341, 342-682, 683-1023
-            display = 'a'
-        elif light_sensor_val < 682:
-            display = 'b'
-        else: # light_sensor_val > 682:
-            display = 'c'
-        client.publish("lizarral/displayinfo", f"{display}")
-        time.sleep(4)
+        time.sleep(1)
+
